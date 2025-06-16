28fbcfc741b6cb18a15644e0a4323f9061a6e641
murtazasmart <murtazasmart@hotmail.com>
2025-06-17 00:09:25 +0530

    feat: make miqaat_id nullable in events table

--- database/migrations/2025_06_16_180715_add_miqaat_id_to_events_table.php
+++ database/migrations/2025_06_16_180715_add_miqaat_id_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->foreignId('miqaat_id')->constrained('miqaats')->onDelete('cascade')->after('id');
+            $table->foreignId('miqaat_id')->nullable()->constrained('miqaats')->onDelete('cascade')->after('id');
         });
     }
 
